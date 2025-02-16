bcf388151bf61cae5e45c39f15f3c288120eef14
Tridominous <jamesmou02@gmail.com>
2025-02-16 21:21:03 +0000

    implement mobile navigation

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+
+export const sidebarLinks = [
+    {
+      imgURL: "/icons/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/icons/users.svg",
+      route: "/team",
+      label: "Team",
+    },
+    {
+      imgURL: "/icons/star.svg",
+      route: "/collections",
+      label: "Collections",
+    },
+    {
+      imgURL: "/icons/tag.svg",
+      route: "/categories",
+      label: "Categories",
+    },
+    {
+      imgURL: "/icons/user.svg",
+      route: "/profile",
+      label: "Profile",
+    },
+  ];
--- constants/routes.ts
+++ constants/routes.ts
@@ -3,6 +3,10 @@ const ROUTES = {
     HOME: '/',
     SIGN_IN: '/sign-in',
     SIGN_UP: '/sign-up',
+    TEAM: "/team",
+    COLLECTION: "/collections",
+    CATEGORIES: "/categories",
+    PROFILE: "/profile",
 }
 
 export default ROUTES;
